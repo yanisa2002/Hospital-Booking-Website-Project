95048d71a1cca295878d6692766ae04d6c3a8cd1
YANISA PONGMETHA <yanisa2002@gmail.com>
2022-11-21 23:35:47 +0700

    to see validate

--- src/page/login.js
+++ src/page/login.js
@@ -1,9 +1,28 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-const login = () => {
-  // const [user, setUser] = useState("");
-  // const [pwd, setPwd] = useState("");
-  // const [errMsg, setErrMsg] = useState("");
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const defaultValues = {
+    Email: email,
+    Password: pwd,
+  };
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    setEmail("");
+    setPwd("");
+  };
 
   return (
     <div className="flex justify-center bg-uColor-bg font-prompt min-w-[800px] h-full">
@@ -24,50 +43,63 @@ const login = () => {
             <label className="block text-gray-darker text-lg font-bold mb-8">
               เข้าสู่บัญชีของคุณ
             </label>
-
-            <div className="mb-10">
-              <label
-                className="block text-gray-darker text-sm font-bold mb-2"
-                for="username"
+            <div className="w-full">
+              <p
+                ref={errRef}
+                className={`${
+                  errMsg ? "errmsg" : "offscreen"
+                } text-red-600 text-md text-center`}
+                aria-live="assertive"
               >
-                ชื่อผู้ใช้
-              </label>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                id="username"
-                type="text"
-                placeholder="Username"
-                //autoComplete="off"
-                //pattern="[A-Za-z0-9]{4,20}"
-                //reduired
-              />
+                {errMsg}
+              </p>
             </div>
-            <div className="mb-10">
-              <label
-                className="block text-gray-daker text-sm font-bold mb-2"
-                for="password"
-              >
-                รหัสผ่าน
-              </label>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                id="password"
-                type="password"
-                placeholder="Password"
-                // onChange={(e) => setPwd(e.target.value)}
-                // value={pwd}
-                // required
-              />
-            </div>
-            {/* <Link to="/"> */}
-            <div className="mb-7">
-              <button
-                type="submit"
-                className="w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition"
-              >
-                เข้าสู่ระบบ
-              </button>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-10">
+                <label
+                  className="block text-gray-darker text-sm font-bold mb-2"
+                  for="email"
+                >
+                  อีเมลผู้ใช้
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                  id="email"
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  pattern="/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i"
+                  reduired
+                />
+              </div>
+              <div className="mb-10">
+                <label
+                  className="block text-gray-daker text-sm font-bold mb-2"
+                  for="password"
+                >
+                  รหัสผ่าน
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => setPwd(e.target.value)}
+                  value={pwd}
+                  required
+                />
+              </div>
+              {/* <Link to="/"> */}
+              <div className="mb-7">
+                <button
+                  type="submit"
+                  className="w-full inline-flex items-center justify-center px-4 py-2 bg-uColor-green border border-transparent rounded-md font-semibold capitalize text-[#ffffff] hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition"
+                >
+                  เข้าสู่ระบบ
+                </button>
+              </div>
+            </form>
             {/* </Link> */}
             {/* </form> */}
 
@@ -85,4 +117,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
--- src/page/register.js
+++ src/page/register.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-function register() {
+const Register = () => {
+  const [formErrors, setFormErrors] = useState({});
+
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  let defaultValues = {
+    Email: email,
+    Password: pwd,
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(defaultValues);
+  };
+
   return (
     <div className="flex justify-center bg-uColor-bg font-prompt min-w-[800px] h-full">
       <div className="grid grid-cols-2">
@@ -20,52 +35,50 @@ function register() {
             <label className="block text-gray-darker text-lg font-bold mb-8">
               สร้างบัญชีของคุณ
             </label>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-10">
+                <label
+                  className="block text-gray-darker text-sm font-bold mb-2"
+                  for="email"
+                >
+                  อีเมลผู้ใช้
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                  id="email"
+                  type="text"
+                  placeholder="Email"
+                  pattern="/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i"
+                  reduired
+                />
+              </div>
+              <div className="mb-10">
+                <label
+                  className="block text-gray-daker text-sm font-bold mb-2"
+                  for="password"
+                >
+                  รหัสผ่าน
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => setPwd(e.target.value)}
+                  value={pwd}
+                  required
+                />
+              </div>
 
-            <div className="mb-10">
-              <label
-                className="block text-gray-darker text-sm font-bold mb-2"
-                for="username"
-              >
-                ชื่อผู้ใช้
-              </label>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                id="username"
-                type="text"
-                placeholder="Username"
-                //autoComplete="off"
-                //pattern="[A-Za-z0-9]{4,20}"
-                //reduired
-              />
-            </div>
-            <div className="mb-10">
-              <label
-                className="block text-gray-daker text-sm font-bold mb-2"
-                for="password"
-              >
-                รหัสผ่าน
-              </label>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                id="password"
-                type="password"
-                placeholder="Password"
-                // onChange={(e) => setPwd(e.target.value)}
-                // value={pwd}
-                // required
-              />
-            </div>
-            {/* <Link to="/"> */}
-            <div className="mb-7">
-              <button
-                type="submit"
-                className="w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition"
-              >
-                สมัคร
-              </button>
-            </div>
-            {/* </Link> */}
-            {/* </form> */}
+              <div className="mb-7">
+                <button
+                  type="submit"
+                  className="w-full inline-flex items-center justify-center px-4 py-2 bg-uColor-green border border-transparent rounded-md font-semibold capitalize text-[#ffffff] active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition"
+                >
+                  สมัคร
+                </button>
+              </div>
+            </form>
 
             <div className="mb-2 text-center">
               มีบัญชีผู้ใช้แล้ว ?{" "}
@@ -79,6 +92,6 @@ function register() {
       </div>
     </div>
   );
-}
+};
 
-export default register;
+export default Register;
